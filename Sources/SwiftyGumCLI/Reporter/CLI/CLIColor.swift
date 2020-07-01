e725a4083da2c254342239057b2d61e0d033267a
mtj0928 <mtj0928@users.noreply.github.com>
2020-07-02 00:37:24 +0900

    Update Color value

--- Sources/SwiftyGumCLI/Reporter/CLI/CLIColor.swift
+++ Sources/SwiftyGumCLI/Reporter/CLI/CLIColor.swift
@@ -2,15 +2,15 @@ import Foundation
 
 enum CLIColor: String {
 
-    case black = "\u{1b}[30m"
-    case red = "\u{1b}[31m"
-    case green = "\u{1b}[32m"
-    case yellow = "\u{1b}[33m"
-    case blue = "\u{1b}[34m"
-    case magenta = "\u{1b}[35m"
-    case cyan = "\u{1b}[36m"
-    case white = "\u{1b}[37m"
-    case none = "\u{1b}[0m"
+    case black      = "\u{001b}[30m"
+    case red        = "\u{001b}[31m"
+    case green      = "\u{001b}[32m"
+    case yellow     = "\u{001b}[33m"
+    case blue       = "\u{001b}[34m"
+    case magenta    = "\u{001b}[35m"
+    case cyan       = "\u{001b}[36m"
+    case white      = "\u{001b}[37m"
+    case none       = "\u{001b}[0m"
 
     func text(_ text: String) -> String {
         "\(self.rawValue)\(text)\(CLIColor.none.rawValue)"
