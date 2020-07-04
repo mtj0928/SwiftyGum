54d0f9c9e3d74de24d738d50ce5b7589ae602db8
mtj0928 <mtj0928@users.noreply.github.com>
2020-07-04 14:50:47 +0900

    Fix encoding bug

--- Sources/SwiftyGumCLI/Reporter/CLI/ColorRange.swift
+++ Sources/SwiftyGumCLI/Reporter/CLI/ColorRange.swift
@@ -1,15 +1,15 @@
 struct ColorRange {
-    let range: Range<String.Index>
+    let range: Range<Int>
     let color: CLIColor
 }
 
 struct CLIString {
-    let text: String
+    let text: String.UTF8View
     private(set) var colorRanges = [ColorRange]()
 
-    init(text: String) {
+    init(text: String.UTF8View) {
         self.text = text
-        colorRanges = [ColorRange(range: text.startIndex..<text.endIndex, color: .none)]
+        colorRanges = [ColorRange(range: 0..<text.count, color: .none)]
     }
 
     mutating func append(_ newObject: ColorRange) {
@@ -56,14 +56,16 @@ struct CLIString {
         var index = 0
         revesed.forEach { colorRange in
             if colorRange.color != .none {
-                text.insert(contentsOf: CLIColor.none.rawValue, at: colorRange.range.upperBound)
+                text.insert(contentsOf: CLIColor.none.rawValue.utf8, at: colorRange.range.upperBound)
             }
 
             // ommision for unedited text
             if colorRange.color == .none {
-                let substring = text[colorRange.range]
+                let startIndex = text.index(text.startIndex, offsetBy: colorRange.range.lowerBound)
+                let endIndex = text.index(text.startIndex, offsetBy: colorRange.range.upperBound)
+                let substring = text[startIndex..<endIndex]
                 var lines = [String]()
-                substring.enumerateLines { (line, stop) in lines.append(line) }
+                substring.enumerateLines { lines.append($0) }
 
                 if index == 0 && lines.count >= 3 {
                     lines = [lines[0], lines[1], lines[2]]
@@ -73,14 +75,45 @@ struct CLIString {
                     lines = [lines[0], lines[1], lines[2], "~~~~~",lines[lines.count - 3], lines[lines.count - 2], lines[lines.count - 1]]
                 }
                 let omittedText = lines.joined(separator: "\n")
-                text.replaceSubrange(colorRange.range, with: omittedText)
+                text.replaceSubrange(colorRange.range, with: omittedText.utf8)
             }
 
             if colorRange.color != .none {
-                text.insert(contentsOf: colorRange.color.rawValue, at: colorRange.range.lowerBound)
+                text.insert(contentsOf: colorRange.color.rawValue.utf8, at: colorRange.range.lowerBound)
             }
             index += 1
         }
-        return text
+        return String(text)
+    }
+}
+
+extension String.UTF8View {
+
+    mutating func insert(contentsOf text: String.UTF8View, at offset: Int) {
+        let index = self.index(self.startIndex, offsetBy: offset)
+        let first = self[self.startIndex..<index]
+        let second = self[index..<self.endIndex]
+        let internalText = text[text.startIndex..<text.endIndex]
+        let substring = Substring(first) + Substring(internalText) + Substring(second)
+        self = String(substring).utf8
+    }
+
+    mutating func replaceSubrange(_ range: Range<Int>, with text: String.UTF8View) {
+        let range = self.index(self.startIndex, offsetBy: range.lowerBound)..<self.index(self.startIndex, offsetBy: range.upperBound)
+        let first = self[self.startIndex..<range.lowerBound]
+        let second = self[range.upperBound..<self.endIndex]
+        let internalText = text[text.startIndex..<text.endIndex]
+        let substring = Substring(first) + Substring(internalText) + Substring(second)
+        self = String(substring).utf8
+    }
+}
+
+extension String.UTF8View.SubSequence {
+
+    func enumerateLines(_ handler: @escaping (String) -> Void) {
+        let string = String(self)
+        string?.enumerateLines { (line, _) in
+            handler(line)
+        }
     }
 }
